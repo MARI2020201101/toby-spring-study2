3808cc521e55acc298e37def1992020d0744a536
MARI <mari20201101@gmail.com>
2021-11-03 00:34:03 +0900

    Learn factory method pattern with spring

--- src/main/java/com/mariworld/spring/scope/ObjectFactoryConfig.java
+++ src/main/java/com/mariworld/spring/scope/ObjectFactoryConfig.java
@@ -1,2 +1,22 @@
-package com.mariworld.spring.scope;public class ObjectFactoryConfig {
+package com.mariworld.spring.scope;
+
+import org.springframework.beans.factory.config.ObjectFactoryCreatingFactoryBean;
+import org.springframework.beans.factory.config.ServiceLocatorFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectFactoryConfig {
+
+    @Bean
+    public ObjectFactoryCreatingFactoryBean scopeBeanFactory(){
+        ObjectFactoryCreatingFactoryBean obj = new ObjectFactoryCreatingFactoryBean();
+        obj.setTargetBeanName("singletonScopeBean");
+        return obj;
+    }
+
+
+
 }
+
+
